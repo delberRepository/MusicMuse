406082e9c94a3e9e80d3a7385c6d5a94871cdf98
IñigoMartin <107561907+delberRepository@users.noreply.github.com>
2025-01-26 23:55:31 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -59,8 +59,8 @@ session_start(); // Asegúrate de que la sesión esté iniciada
 <head>    
     <title>IndieSound</title>    
     <meta charset="UTF-8">
-    <meta name="title" content="Gatos: Amigos Peludos">
-    <meta name="description" content="Descubre todo sobre gatos: razas, cuidados y curiosidades.">  
+    <meta name="title" content="MusicMuse">
+    <meta name="description" content="La App para artistas emergentes">  
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">    
    <link href="estiloPag.css" rel="stylesheet" type="text/css"> 
